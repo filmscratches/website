285b908e9f99dfaf191dee34784c1d968c62696f
Chris Wilson <chriswilson023@hotmail.co.uk>
2022-03-14 18:51:13 +0000

    films?!

--- film/film.js
+++ film/film.js
@@ -26,35 +26,35 @@ function toggleTag(tag) {
 }
 
 function applyFilters() {
-  const books = document.getElementsByClassName('book')
-  for (let i = 0; i < books.length; i++) {
-    const status = books[i].getElementsByClassName('status')[0].textContent
+  const films = document.getElementsByClassName('film')
+  for (let i = 0; i < films.length; i++) {
+    const status = films[i].getElementsByClassName('status')[0].textContent
     let include = true
     if (selectedStatuses.length !== 0 && !selectedStatuses.includes(status)) {
      include = false
     }
-    const bookTags = books[i].getElementsByClassName('taglist')[0].textContent.split(", ")
-    if (selectedTags.length !== 0 && !includeBook(bookTags, selectedTags)) {
+    const filmTags = films[i].getElementsByClassName('taglist')[0].textContent.split(", ")
+    if (selectedTags.length !== 0 && !includefilm(filmTags, selectedTags)) {
       include = false
     }
 
     if (include) {
-      books[i].style.display = 'block'
+      films[i].style.display = 'block'
     }
     else {
-      books[i].style.display = 'none'
+      films[i].style.display = 'none'
     }
   }
 }
 
-function includeBook(bookTags, selectedTags) {
+function includefilm(filmTags, selectedTags) {
   const anyOrAll = document.getElementById("any-all").value
 
   // any overlap is fine
   if (anyOrAll === "any") {
-    for (let i = 0; i < bookTags.length; i++) {
+    for (let i = 0; i < filmTags.length; i++) {
       for (let j = 0; j < selectedTags.length; j++) {
-        if (bookTags[i] === selectedTags[j]) {
+        if (filmTags[i] === selectedTags[j]) {
           return true
         }
       }
@@ -64,7 +64,7 @@ function includeBook(bookTags, selectedTags) {
 
 
   for (let i = 0; i < selectedTags.length; i++) {
-    if (!bookTags.includes(selectedTags[i])) return false
+    if (!filmTags.includes(selectedTags[i])) return false
   }
   return true
 }
